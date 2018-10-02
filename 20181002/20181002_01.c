c80b8b6fb8664e0c6d0fd513371a907ce7905253
GeraldZirkl <43417503+GeraldZirkl@users.noreply.github.com>
2018-10-02 12:14:19 +0200

    Create 20181002_01.c

---
+++ 20181002/20181002_01.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int zahl1 = 5;
+  int zahl2 = 6;
+  int summe = zahl1 + zahl2;
+
+  printf ("Die Summe von %d + %d ist %d\n", zahl1, zahl2, summe);
+
+  return 0;
+}
